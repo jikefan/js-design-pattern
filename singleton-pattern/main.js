f327758cdc9107f9877ebf43fee6079137072ca2
星途000 <xingtu000@dengdeMacBook-Pro.local>
2023-06-05 10:12:59 +0800

    singleton demo

---
+++ singleton-pattern/main.js
@@ -0,0 +1,25 @@
+class Logger {
+    constructor() {
+        if (!Logger.instance) {
+            this.logs = [];
+            this.instance = this;
+        }
+    }
+
+    log(message) {
+        const time = new Date();
+        time.setMilliseconds(time.getMilliseconds() + 8 * 60 * 60 * 1000);
+        const logMsg = `[Logger ${time.toISOString()}] ${message}`;
+        this.logs.push(logMsg);
+        console.log(logMsg);
+    }
+
+    printLogCount() {
+        console.log(`Number of logs: ${this.logs.length}`);
+    }
+}
+
+(function main() {
+    const l = new Logger();
+    l.log('hello world');
+})()
